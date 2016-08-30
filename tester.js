8c6a3ad3b417bc930d0c87390e96f9e7c7059454
Kapil Garg <kapil1garg@gmail.com>
2016-08-30 18:40:43 -0500

    adding testing code

--- tester.js
+++ tester.js
@@ -2,3 +2,19 @@ var supply = require('@dms/supply');
 var jsonFile = require('jsonfile');
 
 var contents = jsonFile.readFileSync('./tests/signals.json');
+var dtw = supply('dtw', {
+    version: '1.0.0' // specify a version
+  });
+
+function dtwCallback(err, res) {
+	console.log('Similarity: ' + res.outputs.similarity);
+}
+
+for (var i in contents) {
+	var data = {
+		query: contents[i].query,
+		template: contents[i].template
+	};
+
+	dtw(data, dtwCallback);
+}
